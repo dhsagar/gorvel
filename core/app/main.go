affe4d06aa5b7d764172d7f738cd0771c650ed31
sadlil <sadlil@thetigerworks.com>
2015-10-08 17:44:50 +0600

    web server adding.

--- core/app/main.go
+++ core/app/main.go
@@ -1,5 +1,17 @@
 package app
+import "github.com/Unknwon/macaron"
 
 func Main() {
-
+	m := macaron.Classic()
+	rendererOptions := macaron.RenderOptions{
+		// Directory to load templates. Default is "templates".
+		Directory: "./../../view",
+		// Extensions to parse template files from. Defaults are [".tmpl", ".html"].
+		Extensions: []string{".tmpl", ".html"},
+		Delims: macaron.Delims{"{{", "}}"},
+	}
+	m.Use(macaron.Renderer(rendererOptions))
+	m.Use(macaron.Static("./../resource"))
+	AddAllRoutes(m)
+	m.Run(8080)
 }
